bbbb9acf5ae1a94e19129af9cbf2901ffa8e29eb
Prajwal Raut <rautprajwal546@gmail.com>
2023-03-27 16:47:15 +0530

    updated tailwind.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-      require('tailwind-scrollbar'),
-    ],
-  }
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
